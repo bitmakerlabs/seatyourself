d71f4dbd76b6f0b63ff733b3864e52fc16db5460
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-23 15:58:14 -0400

    fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+
   get 'restaurants/index'
 
   get 'restaurants/new' => 'restaurant#new'
+  post 'restaurants'=> 'restaurants#create'
+
   get 'restaurants/:id' => 'restaurant#show', as: 'restaurant'
   # get 'restaurants/show'
 

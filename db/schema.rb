843ebff2f33cab4fff5c1409bcaf3f5da43681b8
shari-singh <shari.singh93@gmail.com>
2016-06-23 17:18:18 -0400

    added columns to Reservations. created a form for reservation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623181946) do
+ActiveRecord::Schema.define(version: 20160623211045) do
 
   create_table "diners", force: :cascade do |t|
     t.string   "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20160623181946) do
     t.integer  "restaurant_id"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.datetime "date"
+    t.text     "instructions"
   end
 
   create_table "restaurants", force: :cascade do |t|

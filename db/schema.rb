953a99fb101b246682c1eaf873254f4909591de0
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-23 17:36:44 -0400

    made and migrated Reservation model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160623211158) do
+ActiveRecord::Schema.define(version: 20160623213412) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.datetime "time"
+    t.integer  "party_size"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"

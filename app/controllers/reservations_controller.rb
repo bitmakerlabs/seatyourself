7dc931dcc732ac6bf5e7a3cb7b7b40c8c770c011
Leigha <leigham314@gmail.com>
2016-06-23 15:34:26 -0400

    added methods to all controllers

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,11 +4,42 @@ class ReservationsController < ApplicationController
   end
 
   def new
+    @reservation = Reservation.new
+  end
+
+  def create
+    @reservation = Reservation.new(reservation_params)
+
+    if @reservation.save
+      redirect_to reservations_url
+    else
+      render 'new'
+    end
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
   end
 
   def edit
+    @reservation = Reservation.find(params[:id])
   end
+
+  def update
+    @reservation = Reservation.find(params[:id])
+
+    if @reservation.update_attributes(reservation_params)
+      redirect_to reservations_url(@reservation)
+    else
+      render :edit
+    end
+  end
+
+private
+
+def reservation_params
+  params.require(:reservation).permit(:time, :party_size, :diner_id, :restaurant_id)
+end
+
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,40 @@
 class RestaurantsController < ApplicationController
 
   def new
-  end
-
-  def show
-  end
-
-  def edit
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_url(@restaurant)
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurants_url(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :email, :password_digest)
+
 end

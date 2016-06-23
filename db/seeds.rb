14b9603f0598605d41c3daff00d31f7b3ee5a783
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-23 17:01:49 -0400

    testing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,6 +31,7 @@ Restaurant.create(
 )
 
 
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #

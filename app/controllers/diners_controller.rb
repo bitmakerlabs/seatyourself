669a10e6c3216f0cb68d3d506acc0bdcad87c11c
Leigha <leigham314@gmail.com>
2016-06-23 15:17:50 -0400

    added methods to diner_controller

--- app/controllers/diners_controller.rb
+++ app/controllers/diners_controller.rb
@@ -1,13 +1,42 @@
 class DinersController < ApplicationController
+
   def new
+    @diner = Diner.new
   end
 
   def show
+    @diner = Diner.find(params[:id])
   end
 
   def edit
+    @diner = Diner.find(params[:id])
+  end
+
+  def update
+    @diner = Diner.find(params[:id])
+
+    if @diner.update_attributes(diner_params)
+      redirect_to diner_url(@diner)
+    else
+      render :edit
+    end
   end
 
   def create
+    @diner = Diner.new(diner_params)
+    if @diner.save
+      redirect_to root_url, notice: 'Thanks for signing up!'
+    else
+      render 'new'
+    end
   end
+
+private
+
+def diner_params
+  params.require(:diner).permit(:name, :email, :phone_number, :password_digest)
+end
+
+
+
 end
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+
   def new
   end
 

809b38b2cd0076cb01630be259d41e47630ece0e
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-25 16:48:47 -0400

    created sessions controller methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to restaurants_url, notice: "You've logged in!"
+    else
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to restaurants_url, notice: "You've logged out!"
   end
+
 end

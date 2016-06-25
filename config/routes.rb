82554b1d884d229c9de7e4286c1b93d13407a626
elizabeth <elizabeth.e.epstein@gmail.com>
2016-06-25 16:35:46 -0400

    installed bcrypt gem, created user model and controller, created signup form for user, created log in functionality, created sesssions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # Restaurant
-  # get 'restaurants/index'
+
 
   root 'restaurants#index'
   #When adding custom routes (i.e. confirmation page), it's best to do it
@@ -9,18 +8,6 @@ Rails.application.routes.draw do
 
   resources :restaurants
   resources :reservations
-
-#   post 'restaurants'=> 'restaurants#create'
-#
-#   get 'restaurants/:id' => 'restaurants#show', as: 'restaurant'
-#
-# # Reservation
-#   get 'reservations/new' => 'reservations#new'
-#   post 'reservations' => 'reservations#create'
-
-  #get 'reservations/create
-
-
-
-
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160624212827) do
+ActiveRecord::Schema.define(version: 20160625195634) do
 
   create_table "reservations", force: :cascade do |t|
     t.datetime "date"
@@ -38,6 +38,14 @@ ActiveRecord::Schema.define(version: 20160624212827) do
     t.string   "cuisine"
     t.integer  "opening_time"
     t.integer  "closing_time"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end

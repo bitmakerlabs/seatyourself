05c6ca9a3e171e7cd22de59770e705850516f466
Jack WIlde <jackwilde630@gmail.com>
2016-08-31 16:12:46 -0400

    generate cuisine model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160831200305) do
+ActiveRecord::Schema.define(version: 20160831201141) do
+
+  create_table "cuisines", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.time     "time"

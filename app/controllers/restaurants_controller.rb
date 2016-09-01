567c222f91d5f345c6e28d7a7b7346de3d27fb58
Jack WIlde <jackwilde630@gmail.com>
2016-09-01 14:14:13 -0400

    write new action for restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,37 @@
+class RestaurantsController < ApplicationController
+  def new
+    @restaurant = Restaurant.new
+    @cuisines = Cuisine.all
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render 'new'
+    end
+  end
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def delete
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :Cuisine, :description, :opens_at, :closes_at)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :restaurants
+
   resource :users
   root 'users#new'
 end

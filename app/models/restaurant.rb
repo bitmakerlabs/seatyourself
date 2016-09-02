7c0c48e3e3baf9813fbd5cee508226cd1f564f61
Adam Tse <mr.adam.tse@gmail.com>
2016-09-02 15:12:16 -0400

    model changes resturant for geocoder

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,4 +3,6 @@ class Restaurant < ActiveRecord::Base
   belongs_to :owner, class_name: :user
   has_many :reservations
   has_many :users, through: :reservations
+  geocoded_by :address      #this can be Ip as well
+  after_validation :geocode, if: :address_changed? #auto-fetch coordinates
 end

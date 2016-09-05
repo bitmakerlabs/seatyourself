01b0a9c93d676329fac243f1893eca43abd90f34
Jack WIlde <jackwilde630@gmail.com>
2016-09-05 18:30:03 -0400

    passwords must be 8 chars or more

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash.now[:success] = "You have succesfully created an Account"
-      redirect_to new_session_path
+      redirect_to root_path
     else
       render :new
     end

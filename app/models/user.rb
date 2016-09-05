0311e42d8b213ba3c101b881dee76cfc752ac1f5
Adam Tse <mr.adam.tse@gmail.com>
2016-09-05 16:17:22 -0400

    validations for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ActiveRecord::Base
   has_secure_password
   has_many :reservations
   has_many :restaurants, through: :reservations
+  validates :first_name, :last_name, :email, :password, :password_confirmation, presence: true
 end

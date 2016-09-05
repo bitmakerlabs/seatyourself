9f0dd6e895f8eb72e5a0ee1cdff2559e483efd73
Jack WIlde <jackwilde630@gmail.com>
2016-09-05 16:40:38 -0400

    add cuisine validations

--- app/controllers/cuisines_controller.rb
+++ app/controllers/cuisines_controller.rb
@@ -1,12 +1,16 @@
 class CuisinesController < ApplicationController
+  after_filter "save_my_previous_url", only: [:new]
+
   def new
+    @back_url = session[:my_previous_url]
     @cuisine = Cuisine.new
   end
 
   def create
     @cuisine = Cuisine.new(cuisine_params)
+    @cuisine.name.capitalize!
     if @cuisine.save
-      redirect_to root_path
+      redirect_to new_restaurant_path
     else
       render 'new'
     end
@@ -19,6 +23,10 @@ class CuisinesController < ApplicationController
 
   def cuisine_params
     params.require(:cuisine).permit(:name)
+  end
+
+  def save_my_previous_url
+    session[:my_previous_url] = URI(request.referer || '').path
   end
 
 end
--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,3 +1,5 @@
 class Cuisine < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, uniqueness: true
   has_many :restaurants
 end

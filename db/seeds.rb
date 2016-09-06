76cdc98a1b1a357612d15d0d1e68191aa48d724c
Jack WIlde <jackwilde630@gmail.com>
2016-09-06 11:04:33 -0400

    fix some views

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ Cuisine.create(
 
 Restaurant.create(
   name: "El Caballito",
-  address: "220 King St W",
+  address: "220 King St W, Toronto",
   phone_number: "4162123456",
   cuisine_id: 1,
   description: "Loungey hot spot offering a big tequila selection, margaritas & a full menu of Mexican street fare.",
@@ -36,7 +36,7 @@ Restaurant.create(
 
 Restaurant.create(
   name: "Burrito Boyz",
-  address: "224 Adelaide St W",
+  address: "224 Adelaide St W, Toronto",
   phone_number: "4168546547",
   cuisine_id: 1,
   description: "Mexican outlet with limited seating offering sizable meat, seafood & vegetarian burritos.",
@@ -48,7 +48,7 @@ Restaurant.create(
 
 Restaurant.create(
   name: "Gallery Sushi",
-  address: "275 Dundas Street W",
+  address: "275 Dundas Street W, Toronto",
   phone_number: "4168548754",
   cuisine_id: 2,
   description: "Casual standby offering creative sushi rolls & other Japanese fare in simple environs.",

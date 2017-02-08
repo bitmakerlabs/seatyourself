f9bd2488f43996d4f0ae05393c2d53ae4ebad70c
Dgibbins <nayr_ryan@hotmail.com>
2017-02-08 16:14:41 -0500

    Migrate add_location

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208192650) do
+ActiveRecord::Schema.define(version: 20170208194847) do
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "time"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20170208192650) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|

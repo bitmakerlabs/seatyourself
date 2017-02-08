108104c83ccdcea27671c7dfce2e60e6778e89bd
drhie <rhie.david@gmail.com>
2017-02-08 14:36:27 -0500

    Add seeds for categories and restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Category.create(name: "asian")
+Category.create(name: "italian")
+Category.create(name: "american")
+Restaurant.create(
+  name: "Chico Resto",
+  description: "This is an opium den with great nachos!",
+  capacity: 10,
+  category_id: 3,
+)
+Restaurant.create(
+  name: "Butt Fuggers",
+  description: "This is an upscale restaurant with high-class clientele.",
+  capacity: 10,
+  category_id: 2,
+)
+Restaurant.create(
+  name: "Pudding",
+  description: "Exotic Indian cuisine and serving all your pudding needs since 1963.",
+  capacity: 10,
+  category_id: 1,
+)
+Restaurant.create(
+  name: "Day Ol' Discount",
+  description: "Cheap eats like you've never seen before! The food's a day old, too",
+  capacity: 10,
+  category_id: 3,
+)

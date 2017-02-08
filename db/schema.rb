e933b80abee23fea44fdf93fa22c55e036a4fd8c
drhie <rhie.david@gmail.com>
2017-02-08 14:42:20 -0500

    Remove ttime from restaurants too 'cause we made mistake

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208193906) do
+ActiveRecord::Schema.define(version: 20170208194142) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20170208193906) do
     t.integer  "category_id"
     t.integer  "opening"
     t.integer  "closing"
-    t.datetime "ttime"
   end
 
   create_table "users", force: :cascade do |t|

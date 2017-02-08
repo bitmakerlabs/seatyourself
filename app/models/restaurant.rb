d27a07ae3c141e550866a6ad60e918cdeba00621
drhie <rhie.david@gmail.com>
2017-02-08 14:49:53 -0500

    Add validations and location column to restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,4 +2,5 @@ class Restaurant < ApplicationRecord
   belongs_to :category
   has_many :reservations
   has_many :users, through: :reservations
+  validates :name, :capacity, :location, :opening, :closing, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170208194142) do
+ActiveRecord::Schema.define(version: 20170208194847) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170208194142) do
     t.integer  "category_id"
     t.integer  "opening"
     t.integer  "closing"
+    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|

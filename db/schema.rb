71ad7f63543e5b9aa00404a12f8fb940731b2ddb
Dgibbins <nayr_ryan@hotmail.com>
2017-02-08 17:08:50 -0500

    Seed Users

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 20170208194847) do
 
   create_table "categories", force: :cascade do |t|
@@ -19,7 +17,6 @@ ActiveRecord::Schema.define(version: 20170208194847) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 
   create_table "reservations", force: :cascade do |t|
     t.integer  "time"
@@ -37,13 +34,10 @@ ActiveRecord::Schema.define(version: 20170208194847) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-<<<<<<< HEAD
+    t.string   "location"
     t.integer  "category_id"
     t.integer  "opening"
     t.integer  "closing"
-=======
->>>>>>> ryan
-    t.string   "location"
   end
 
   create_table "users", force: :cascade do |t|

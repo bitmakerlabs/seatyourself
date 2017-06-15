5a508bfddf98c78ba40c89f027b4067c7c53477d
DocPrior <Sara Prior>
2017-06-15 17:05:59 -0400

    create db seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170615191420) do
+ActiveRecord::Schema.define(version: 20170615192247) do
 
   create_table "diners", force: :cascade do |t|
     t.string   "name"
@@ -53,6 +53,11 @@ ActiveRecord::Schema.define(version: 20170615191420) do
     t.datetime "updated_at",      null: false
     t.index ["owner_id"], name: "index_restaurants_on_owner_id"
     t.index ["reservations_id"], name: "index_restaurants_on_reservations_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,90 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Diner.create(
+  name: 'Sara',
+  contact_info: 'email, 123456',
+  reservations_id: 1
+)
+
+Diner.create(
+  name: 'Emily',
+  contact_info: 'email2, 789123',
+  reservations_id: 2
+)
+
+Diner.create(
+  name: 'Vrunda',
+  contact_info: 'email3, 567890',
+  reservations_id: 3
+)
+
+Owner.create(
+  name: 'Joe',
+  restaurant_id: 1
+)
+
+Owner.create(
+  name: 'Bob',
+  restaurant_id: 2
+)
+Owner.create(
+  name: 'Mike',
+  restaurant_id: 3
+)
+
+Restaurant.create(
+  name: 'Bench',
+  address: 'King',
+  capacity: 50,
+  open_time: 12,
+  close_time: 23,
+  reservations_id: 1,
+  owner_id: 1
+)
+
+Restaurant.create(
+  name: 'Eat it!',
+  address: 'Queen',
+  capacity: '25',
+  open_time: 10,
+  close_time: 17,
+  reservations_id: 2,
+  owner_id: 2
+)
+
+Restaurant.create(
+  name: 'Hungry Racoon',
+  address: 'Bloor',
+  capacity: '100',
+  open_time: 00,
+  close_time: 10,
+  reservations_id: 3,
+  owner_id: 3
+)
+
+Reservation.create(
+  date: '063017',
+  time: 13,
+  party_size: 4,
+  diner_id: 1,
+  restaurant_id: 1
+)
+
+Reservation.create(
+  date: '062517',
+  time: 15,
+  party_size: 10,
+  diner_id: 2,
+  restaurant_id: 2
+)
+
+Reservation.create(
+  date: '062017',
+  time:
+  9,
+  party_size: 2,
+  diner_id: 3,
+  restaurant_id: 3
+)

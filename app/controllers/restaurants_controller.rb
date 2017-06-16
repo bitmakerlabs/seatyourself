5148a2ad955059b3e35b90382890687ea6aaa927
vrunda2712 <angy.v27@gmail.com>
2017-06-16 13:11:55 -0400

    view to display list of restaurant is been created

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,27 @@
 class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def new
+
+  end
+
+  def show
+    
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
 end

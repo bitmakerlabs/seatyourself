ac0e5f19685e37340e1d8042a0b9c14927fed4bf
vrunda2712 <angy.v27@gmail.com>
2017-06-17 11:05:07 -0400

    changes made to solve errors and added controllers for restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,23 +5,45 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+  end
 
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      redirect_to new_restaurant_path
+    end
   end
 
   def edit
-
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
-
+    @restaurant = Restaurant.find(params[:id])
+    if @restaurant.update(restaurant_params)
+      redirect_to @restaurant
+    else
+      redirect_back_or_to @restaurant
+    end
   end
 
   def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
 
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :capacity, :open_time, :close_time, :url)
   end
 
 end

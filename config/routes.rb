1e720493aa21b3dfd5115ecdc06c3a0d5f5e4da3
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-24 16:40:15 -0400

    setup the users controller to save a new user instance

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
 
@@ -6,4 +8,6 @@ Rails.application.routes.draw do
     resources :reservations
   end
   resources :reservations, only: :index
+
+  resources :users, only: [:new, :create]
 end

49275ccce98eb64310345f061f47168866fc4433
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-24 14:10:26 -0400

    created the mehtods in the restaurant controller and setup the before action for find restaurant and new restaurant

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,43 @@
 class RestaurantsController < ApplicationController
+before_action :new_restaurant, only:[:new, :create]
+before_action :find_restaurant, only: [:show, :edit, :update, :destroy]
+
   def index
   end
 
   def show
+    #find_restaurant
+  end
+
+  def new
+    #new_restaurant
+  end
+
+  def create
+    #new_restaurant
+    #find_restaurant
+  end
+
+  def edit
+    #find_restaurant
+  end
+
+  def update
+    #find_restaurant
+  end
+
+  def destroy
+    #find_restaurant
+  end
+
+  private
+
+  def new_restaurant
+    @restaurant = Restaurant.new
+  end
+
+  def find_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+
 end

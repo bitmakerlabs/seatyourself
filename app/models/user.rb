e0a65de97cf3b43f3430e60525508397632dada0
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-24 23:30:42 -0400

    commented log out as it does not work and added validations to our models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :reservations
-  has_many :restaurants
+  has_many :restaurants, through: :reservations
+
+  validates :name, :email, :phone, presence: true
+  validates :email, uniqueness: true
 end

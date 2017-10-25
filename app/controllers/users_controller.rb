1e5a4a084488c2d0a28702b1820b747bf2edee5c
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-25 00:58:26 -0400

    added comments to the user controller where i made a change to the code so the session:user_id is set properly so we can log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
+      session[:user_id] = @user.id # this will remember and keep the user logged in by saving the info in their session cookie
       flash[:notice] = "Thank you for signing up"
       redirect_to root_path
     else

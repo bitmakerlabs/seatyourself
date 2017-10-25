afa0a0daad8ea432c3e9d718f0c04027146e216f
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-10-25 00:41:12 -0400

    added the css rule for error message highlighting, fixed our helper methods, found a bug where we were not passing the user id to the session params in the user create method as we needed to save it into an instance variable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,27 @@
 class SessionsController < ApplicationController
+
+  def destroy
+    session[:user_id] = nil
+    # to log the user out, which clears the cookie
+    # session.clear
+    redirect_to root_url
+  end
+
   def new
+    # this stays empty becuase we are only showing the login form
   end
 
   def create
     user = User.find_by(email: params[:session][:email])
+
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Thank you for logging in"
-      redirect_to root_path
+      redirect_to root_url
     else
-      flash[:alert] = "Nice try succa!"
+      flash[:alert] = "Email or Password Incorrect"
       render :new
     end
+
   end
 
-  def destroy
-    session.clear
-    redirect_to root_path
-  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # @session = session[:user_id]
+      session[:user_id] = @user.id
       flash[:notice] = "Thank you for signing up"
       redirect_to root_path
     else
@@ -19,5 +19,4 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password)
   end
-
 end

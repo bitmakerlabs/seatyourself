5ae2323bdb4fdc6a5cf495777c4eae06dec8703f
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-05 12:33:13 -0500

    ensure user owns restaurant/reservation before accepting changes

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,17 @@
 class ReservationsController < ApplicationController
-  before_action :ensure_logged_in 
+  before_action :ensure_logged_in
+  before_action :load_reservation, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_user_owns_reservation
+
+def load_reservation
+  @reservation = Reservation.find(params[:id])
+end
+
+def ensure_user_owns_reservation
+  unless current_user == @reservation.user
+    flash[:alert] = "Please log in"
+    redirect_to new_sessions_url
+  end
+end
 
 end

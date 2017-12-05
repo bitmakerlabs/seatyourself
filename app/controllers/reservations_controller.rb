73669ca038b3137f6925060426575dd9002aecd0
Situ William <situ_william@yahoo.com>
2017-12-05 14:56:22 -0500

    fixed reservation error

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,14 +1,15 @@
 class ReservationsController < ApplicationController
   before_action :ensure_logged_in
   before_action :load_reservation, only: [:show, :edit, :update, :destroy]
-  before_action :ensure_user_owns_reservation
+  before_action :ensure_user_owns_reservation, except: [:new, :create]
   
   def load_reservation
     @reservation = Reservation.find(params[:id])
   end
 
   def ensure_user_owns_reservation
-    unless current_user.id == @reservation.user_id
+  	@reservations = Reservation.find_by(user_id: params[:user_id])
+    unless current_user.id == @reservations.user_id
       flash[:alert] = "Please log in"
       redirect_to new_sessions_url
     end

bbd3ddc3f1c53f4082a47787b3c9108dfe47c88e
bluepeartree <sherrie.sebastian@gmail.com>
2017-12-06 12:44:56 -0500

    add reservation functionality works

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -36,7 +36,7 @@ class ReservationsController < ApplicationController
     @reservation = Reservation.new
     @reservation.user_id = current_user.id
     @reservation.restaurant_id = params[:reservation][:restaurant_id]
-    @reservation.date_time = Time.local(params[:reservation]["date_time(1i)"].to_i,
+    @reservation.date_time = Time.utc(params[:reservation]["date_time(1i)"].to_i,
                                         params[:reservation]["date_time(2i)"].to_i,
                                         params[:reservation]["date_time(3i)"].to_i,
                                         params[:reservation]["date_time(4i)"].to_i,
@@ -60,7 +60,7 @@ class ReservationsController < ApplicationController
   def update
     @reservation.user_id = current_user.id
     @reservation.restaurant_id = params[:reservation][:restaurant_id]
-    @reservation.date_time = Time.local(params[:reservation]["date_time(1i)"].to_i,
+    @reservation.date_time = Time.utc(params[:reservation]["date_time(1i)"].to_i,
                                         params[:reservation]["date_time(2i)"].to_i,
                                         params[:reservation]["date_time(3i)"].to_i,
                                         params[:reservation]["date_time(4i)"].to_i,

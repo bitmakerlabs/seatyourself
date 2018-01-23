6f5084126db0a22e2d189d7ddc2111b456044cac
jeffrothwell <rothwelljeff@gmail.com>
2018-01-23 16:17:00 -0500

    seed some users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180123200055) do
+ActiveRecord::Schema.define(version: 20180123210747) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "user_id"
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20180123200055) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
-    t.string   "type"
+    t.string   "kind"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,13 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+user1 = User.create(email: "jeff@rothwell.com", kind: "owner", password: "password")
+user2 = User.create(email: "scott@huston.com", kind: "owner", password: "password")
+user3 = User.create(email: "danny@chang.com", kind: "owner", password: "password")
 
-Restaurant.create(name: "Jeff's Burrito Joint", description: "Good burritos",
+user1.create_restaurant(name: "Jeff's Burrito Joint", description: "Good burritos",
   open_time: Time.new(2018, 1, 23, 13), close_time: Time.new(2018, 1, 23, 23), capacity: 20,
-  price: 3, user_id: 1)
-Restaurant.create(name: "Scott's Rib House", description: "Cheap, mediocre ribs.",
+  price: 3)
+user2.create_restaurant(name: "Scott's Rib House", description: "Cheap, mediocre ribs.",
   open_time: Time.new(2018, 1, 23, 11), close_time: Time.new(2018, 1, 23, 22), capacity: 30,
-  price: 1, user_id: 2)
-Restaurant.create(name: "Danny's Poutine and Fish-fry", description: "Fried fish and potatoes",
+  price: 1)
+user3.create_restaurant(name: "Danny's Poutine and Fish-fry", description: "Fried fish and potatoes",
   open_time: Time.new(2018, 1, 23, 7), close_time: Time.new(2018, 1, 23, 21), capacity: 200,
-  price: 4, user_id: 3)
+  price: 4)

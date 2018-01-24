078a0630d0162ff882b480484d398302b27820a5
jeffrothwell <rothwelljeff@gmail.com>
2018-01-24 18:57:39 -0500

    validate booking - date cannot be in the past

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,12 +2,18 @@ class Booking < ApplicationRecord
   belongs_to :restaurant
   belongs_to :user
 
-  validate :cannot_exceed_capacity
+  validate :cannot_exceed_capacity, :cannot_be_in_the_past
 
   def cannot_exceed_capacity
     people_in_current_time_slot = restaurant.bookings.where(day: day, time: time).sum(:n_people)
     if self.n_people > (restaurant.capacity - people_in_current_time_slot)
-      errors.add(:n_people, "Not enough room for #{self.n_people} people at that time")
+      errors.add(:custom_message, "Not enough room for #{self.n_people} people at that time")
+    end
+  end
+
+  def cannot_be_in_the_past
+    if self.day < Time.now.midnight
+      errors.add(:custom_message, "Date cannot be in the past.")
     end
   end
 end

654c7748b61d5658da0f7ea278b50c83f5dc99a8
Scott <scotthuston1982@gmail.com>
2018-01-24 13:28:21 -0500

    added validations for restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,13 @@
 class Restaurant < ApplicationRecord
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :open_time, presence: true
+  validates :close_time, presence: true
+  validates :price, presence: true
+  validates :capacity, presence: true
+
+
   belongs_to :user
   has_many :bookings
 end

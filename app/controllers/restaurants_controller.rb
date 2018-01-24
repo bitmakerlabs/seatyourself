2619a572606ce114b730f2440878a0923ace7c1b
Scott <scotthuston1982@gmail.com>
2018-01-24 11:01:18 -0500

    added create restaurant form

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,11 +9,20 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new
+    @restaurant.name = params[:restaurant][:name]
+    @restaurant.description = params[:restaurant][:description]
+    @restaurant.open_time = params[:restaurant][:open_time]
+    @restaurant.close_time = params[:restaurant][:close_time]
+    @restaurant.price = params[:restaurant][:price]
 
+    if @restaurant.save
+      redirect_to restaurants_url
+    end
   end
 
   def edit
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,11 +9,13 @@ class UsersController < ApplicationController
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
-
-    @user.kind = "user"
+    @user.kind = params[:user][:kind]
 # Differentiating from kinds will come later
     if @user.save
-      redirect_to root_url
+      if @user.kind == "owner"
+        redirect_to restaurant_form_url
+      else
+        redirect_to root_url
     else
       render :new
     end

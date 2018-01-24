e14bb42acbb4445849c6fdf8c05d0b12faad16d9
Scott <scotthuston1982@gmail.com>
2018-01-24 15:02:39 -0500

    eddited restaurant controller to setup an edit page

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -31,11 +31,24 @@ class RestaurantsController < ApplicationController
   end
 
   def edit
-
+    @restaurant = Restaurant.find(params[:id])
   end
 
   def update
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.name = params[restaurant][:name]
+    @restaurant.description = params[:restaurant][:description]
+    @restaurant.open_time = params[:restaurant][:open_time]
+    @restaurant.close_time = params[:restaurant][:closed_time]
+    @restaurant.price = params[:restaurant][:price]
+    @restaurant.capacity = params[:restaurant][:capacity]
 
+    if @restaurant.save
+      flash[:notice] = "Changes have been saved"
+      redirect_to restaurant_url
+    else
+      render :edit
+    end
   end
 
   def destroy

aca94c5071e2036b52f8b2e23658143029e1cbb6
jeffrothwell <rothwelljeff@gmail.com>
2018-01-24 11:28:57 -0500

    create bookings controller, new booking form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -3,6 +3,13 @@ class BookingsController < ApplicationController
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @booking = @restaurant.bookings.new
+    @booking.time = params[:booking][:time] + params[:hour].hour
+    @booking.n_people = params[:booking][:time]
+    @booking.user = current_user
+
+    if @booking.save
+      
+    end
   end
 
 end

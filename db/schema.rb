0a8c7567696aa7e26d1b02abb20d502a3683d2df
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-26 17:32:43 -0400

    added reservation model, controller and form.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626191220) do
+ActiveRecord::Schema.define(version: 20180626200503) do
+
+  create_table "reservations", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "restaurant_id"
+    t.integer  "partysize"
+    t.date     "date"
+    t.time     "time"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "address"

a8b62bf5ae06d86b3804e6fb0c63bb2ca4fe2415
YUMEE <yumeehk@hotmail.com>
2018-06-26 14:52:03 -0400

    Generate models for user, reservation, restaurant, and controllers for users, reservations, restaurants and sessions.

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,25 @@
+class RestaurantsController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+
+
+  def ensure_user_owns_restaurant
+  end
+
+end 

cdbf59b6e9e3705d6101d27170d35cc64112a0ad
Israelistic <haguyle@gmail.com>
2018-06-27 11:42:00 -0400

    revert to origin and createra new conroller with menual pages

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,13 +1,2 @@
 class ReservationsController < ApplicationController
-  def new_restaurant_reservation
-  end
-
-  def edit_restaurant_reservation
-  end
-
-  def restaurant_reservation
-  end
-
-  def restaurant_reservations
-  end
 end
--- test/controllers/reservations_controller_test.rb
+++ test/controllers/reservations_controller_test.rb
@@ -1,24 +1,7 @@
 require 'test_helper'
 
 class ReservationsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new_restaurant_reservation" do
-    get reservations_new_restaurant_reservation_url
-    assert_response :success
-  end
-
-  test "should get edit_restaurant_reservation" do
-    get reservations_edit_restaurant_reservation_url
-    assert_response :success
-  end
-
-  test "should get restaurant_reservation" do
-    get reservations_restaurant_reservation_url
-    assert_response :success
-  end
-
-  test "should get restaurant_reservations" do
-    get reservations_restaurant_reservations_url
-    assert_response :success
-  end
-
+  # test "the truth" do
+  #   assert true
+  # end
 end

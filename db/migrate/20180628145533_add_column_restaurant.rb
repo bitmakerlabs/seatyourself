ffb078b3afe6b407c3169687c5c8a2feb10c8349
hibernatingnerd <jeffery.mark.campbell@gmail.com>
2018-06-28 12:34:58 -0400

    add time as an integer, change reservation table :time to integer

---
+++ db/migrate/20180628145533_add_column_restaurant.rb
@@ -0,0 +1,9 @@
+class AddColumnRestaurant < ActiveRecord::Migration[5.0]
+  def up
+    change_column :reservations, :time, :integer
+  end
+
+  def down
+    change_column :reservations, :time, :time
+  end
+end

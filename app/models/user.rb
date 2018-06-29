bbbb2169b3525995ebc470001dbf14fb0e6b5259
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-28 20:53:08 -0400

    Add logic to user show page to display based on if there are reservations to display. Add to user#create to sign user in when they sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :restaurants, through: :reservations
   has_many :owned_restaurants, class_name: "Restaurant"
 
-  validates :email, :password_digest, presence: true
+  validates :first_name, :last_name, :email, :password_digest, presence: true
 
 end

85da1e6ccb05685a388d788c26d767852adfc238
Kevin LeBlanc <YOUR EMAIL ADDRESS>
2018-06-28 22:23:49 -0400

    Add restaurant name and hours of opperation to reservation/new view

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -6,9 +6,18 @@ class Reservation < ApplicationRecord
 
   validate :date_not_in_past
 
+  validate :not_open
+
   def date_not_in_past
     if date < Date.today
-      errors.add(:date, "can't be in the past")
+      errors.add(:date, "Date can't be in the past")
     end
   end
+
+  # Trying to validate that reservation is when restaurant is open
+  # def not_open
+  #   if time < open_at || time > close_at
+  #     errors.add(:date, "Restaurant not open at that time")
+  #   end
+  # end
 end

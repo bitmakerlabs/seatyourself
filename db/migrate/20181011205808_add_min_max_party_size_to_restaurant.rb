6ba52020fd1f7b7cfc5e103f3a63b344fd31ba94
kevintudiep <kevintudiep@gmail.com>
2018-10-12 09:23:33 -0500

    added css layout

--- db/migrate/20181011205808_add_min_max_party_size_to_restaurant.rb
+++
@@ -1,6 +0,0 @@
-class AddMinMaxPartySizeToRestaurant < ActiveRecord::Migration[5.2]
-  def change
-    add_column :restaurants, :min_party_size, :integer
-    add_column :restaurants, :max_party_size, :interger
-  end
-end

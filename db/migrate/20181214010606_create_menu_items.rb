4e19b5ad33170c0541d8d498b26ec0d6dedae9bc
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2018-12-13 20:11:57 -0500

    generate models

---
+++ db/migrate/20181214010606_create_menu_items.rb
@@ -0,0 +1,11 @@
+class CreateMenuItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :menu_items do |t|
+      t.integer :restaurant_id
+      t.string :dish
+      t.integer :price
+
+      t.timestamps
+    end
+  end
+end

7a692f133fe7373ce80eed44aea9486999e7b7d9
Benjamin <hoppeconnect@gmail.com>
2018-12-15 15:34:26 -0500

    Fixing restaurant form

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,15 +5,10 @@ class Restaurant < ApplicationRecord
     validates :address, presence: true
     validates :category, presence: true
     validates :neighbourhood, presence: true
-    validates :open_from, presence: true
-    # validates :open_from, numericality: true
-    validates :open_until, presence: true
-    # validates :open_until, numericality: true
     validates :price_range, presence: true
     validates :price_range, numericality: true
     validates :capacity, presence: true
     validates :capacity, numericality: true
     validates :max_reservation_size, presence: true
     validates :max_reservation_size, presence: true
-    #validates :name and address, uniqueness: true? wtf
   end
